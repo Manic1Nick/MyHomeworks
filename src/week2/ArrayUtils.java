b7b84f23e8b4f8b4e6d454b4d65e94a59f1be743
Nick <nick.no.cave@gmail.com>
2016-03-05 00:09:31 +0200

    my homework week2, ArrayUtils + tasks for testing ArrayUtils + adding tasks

--- src/week2/ArrayUtils.java
+++ src/week2/ArrayUtils.java
@@ -29,23 +29,7 @@ public class ArrayUtils {
 
 
     /*1) найти минимальное и максимальное значения в массиве и вывести их на консоль*/
-/*
-    public static void findMinMax(int[] mas) {
 
-	// todo separate big method by two findMinIndex, findMaxIndex
-
-        int numMax = mas[0];
-        int numMin = mas[0];
-
-        for (int i = 0; i < mas.length; i++) {
-            numMax = Math.max(numMax, mas[i]);
-            numMin = Math.min(numMin, mas[i]);
-        }
-
-        // todo method must return result
-        System.out.println("Max number is: " + numMax + "\nMin number is: " + numMin);
-    }
-*/
     public static void findMinAndMax(int[] mas) {
 
         int minIndex = ArrayUtils.findMinIndex(mas);
@@ -79,33 +63,6 @@ public class ArrayUtils {
 
 
     /*2) Поменять местами наибольший и наименьший элементы в массиве*/
-/*
-    // todo see below comments
-    public static int[] replaceMinMax(int[] mas) {
-
-        int numMax = mas[0];
-        int numMin = mas[0];
-        int iMax = 0;
-        int iMin = 0;
-
-        for (int i = 0; i < mas.length; i++) {
-            numMax = Math.max(numMax, mas[i]);
-            numMin = Math.min(numMin, mas[i]);
-        }
-        for (int i = 0; i < mas.length; i++) {
-            if (numMax == mas[i]) {
-                iMax = i;
-            }
-            if (numMin == mas[i]) {
-                iMin = i;
-            }
-        }
-        mas[iMax] = numMin;
-        mas[iMin] = numMax;
-
-        return mas;
-    }
-*/
 
     public static int[] replaceMinMax(int[] mas) {
 
@@ -131,18 +88,6 @@ public class ArrayUtils {
 
     /*3) Заданы два массива одинаковой длины с любыми значениями
     скопировать данные из первого массива во второй*/
-
-    // todo check input arguments, it could be null or empty
-/*
-    public static int[] copyMas1ToMas2(int[] mas1, int[] mas2) {
-
-        for (int i = 0; i < mas1.length; i++) {
-            mas2[i] = mas1[i];
-        }
-
-        return mas2;
-    }
-*/
 
     public static int[] copyMas1ToMas2(int[] mas1, int[] mas2) {
 
@@ -177,37 +122,6 @@ public class ArrayUtils {
 
 
     /*5) Заполнить массив случайными значениями. На четных индексах парные значения, на нечетных непарные*/
-/*
-    public static int[] generateMasEvenRandomOnEvenIndex(int size, int range) {
-
-        int[] mas = new int[size];
-        int random = 0;
-
-        for (int i = 0; i < mas.length; i++) {
-            random = (int) (Math.random() * range);
-            if (i == 0 || i %2 == 0) {
-
-                // todo use ternary operator
-                if (random %2 == 0) {
-                    mas[i] = random;
-                } else {
-                    mas[i] = random + 1;
-                }
-
-            } else {
-
-                //todo use ternary operator
-                if (random %2 != 0) {
-                    mas[i] = random;
-                } else {
-                    mas[i] = random + 1;
-                }
-            }
-        }
-        return mas;
-    }
-*/
-
 
     public static int[] generateMasEvenRandomOnEvenIndex(int size, int range) {
 
@@ -227,20 +141,6 @@ public class ArrayUtils {
 
 
     /*6) Найти среднее арифметическое массива*/
-/*
-    public static int resAverageOfMas(int[] mas) {
-
-        int sum = 0;
-
-        for (int i = 0; i < mas.length; i++) {
-            sum += mas[i];
-        }
-
-        // do operatrion in return statement, we dont need variable in this place
-        int resAverage = sum / mas.length;
-        return resAverage;
-    }
-*/
 
     public static int findAverageOfMas(int[] mas) {
 
@@ -257,33 +157,6 @@ public class ArrayUtils {
 
     /*7) Вывести в консоль элементы той половины одномерного массива
     у которой среднее арифметическое максимальное*/
-/*
-    public static void averageOfHalfMas(int[] mas) {
-        int sum1 = 0;
-        int sum2 = 0;
-        int halfMas = mas.length / 2;
-
-        for (int i = 0; i < halfMas; i++) {
-            sum1 += mas[i];
-        }
-
-        for (int i = halfMas; i < mas.length; i++) {
-            sum2 += mas[i];
-        }
-
-        if (sum1 / halfMas > sum2 / halfMas) {
-          // todo create method that iterate over array with defined start and end ranges
-            for (int i = 0; i < halfMas; i++) {
-                System.out.print(mas[i] + ", ");
-            }
-        } else {
-          // todo create method that iterate over array with defined start and end ranges
-            for (int i = halfMas; i < mas.length; i++) {
-                System.out.print(mas[i] + ", ");
-            }
-        }
-    }
-*/
 
     public static void averageOfHalfMas(int[] mas) {
         int sum1 = 0;
@@ -316,18 +189,6 @@ public class ArrayUtils {
 
     /*8) Eсть два массива одинаковой длины arr1 и arr2 вывести в консоль значения массива,
 	   который получается в результате суммы arr1[i] + arr2[i]*/
-/*
-    // todo before logic, check arrays length
-    public static void summArr1Arr2(int[] arr1, int[] arr2) {
-
-        int[] arr3 = new int[arr1.length];
-
-        for (int i = 0; i < arr1.length; i++) {
-            arr3[i] = arr1[i] + arr2[i];
-            System.out.print(arr3[i] + " ");
-        }
-    }
-*/
 
     public static void summArr1Arr2(int[] arr1, int[] arr2) {
 
@@ -348,50 +209,6 @@ public class ArrayUtils {
 
     /*9) Задать два массива случайными числами от 25 до 75.
     Определить у какого из массивов больше парных елементов.*/
-/*
-    public static boolean evenElementsInMas(int size1, int size2) {
-
-        int[] mas1 = new int[size1];
-        int[] mas2 = new int[size2];
-        int even1 = 0;
-        int even2 = 0;
-
-        for (int i = 0; i < mas1.length; i++) {
-            mas1[i] = (int) (Math.random() * 50) + 25;
-            if (mas1[i] % 2 == 0) {
-                even1++;
-            }
-        }
-        for (int i = 0; i < mas2.length; i++) {
-            mas2[i] = (int) (Math.random() * 50) + 25;
-            if (mas2[i] % 2 == 0) {
-                even2++;
-            }
-        }
-
-      // use pring array method
-        System.out.print("mas1 is: ");
-        for (int i = 0; i < mas1.length; i++) {
-            System.out.print(mas1[i] + ", ");
-        }
-
-        System.out.print("\nmas2 is: ");
-      // use pring array method
-        for (int i = 0; i < mas2.length; i++) {
-            System.out.print(mas2[i] + ", ");
-        }
-
-        System.out.println("\nEven numbers in mas1 more than mas2?");
-
-     // use ternaty operator
-        if (even1 > even2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-*/
-
 
     public static boolean evenElementsInMas(int size1, int size2) {
 
@@ -428,19 +245,6 @@ public class ArrayUtils {
 
     /*10) public static int[] splitArray(int[] arr, int start, int end)
     { // обрезать массив по границам start и end}*/
-/*
-    // check input arguments
-    public static int[] splitArray(int[] arr, int start, int end) {
-
-        int[] split = new int[end - start];
-
-        for(int i = 0; i < split.length; i++) {
-            split[i] = arr[start + i];
-        }
-        return split;
-    }
-*/
-
 
     public static int[] splitArray(int[] arr, int start, int end) {
 
